5918d51bd528635a9a000d17250a1a9b893a4b5e
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-10-16 09:29:22 -0700

    initial commit

---
+++ epic/art-gallery.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="main page for epic"/>
+		<link href="" rel="stylesheet" type="text/css"/>
+		<title>Contact Sheet Wireframe</title>
+	</head>
+	<body>
+		<header>
+			<!-- Navbar -->
+			<nav class="topnav">
+				<a href="index.php">Home</a>
+				<a href="art-gallery.php">Art Gallery - Wireframe</a>
+			</nav>
+		</header>
+		<main>
+			<img src="images/art-gallery-0.png"/>
+			<img src="images/art-gallery-1.png"/>
+			<img src="images/art-gallery-2.png"/>
+			<img src="images/art-gallery-3.png"/>
+		</main>
+	</body>
+	<footer>
+
+	</footer>
+</html>
