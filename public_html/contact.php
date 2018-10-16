41e81f8f118a34f91c39865d849c5122652a4651
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-10-16 12:07:56 -0700

    rearranged page layout and added sticky footer with social media assets

--- public_html/contact.php
+++ public_html/contact.php
@@ -8,40 +8,51 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+		<!-- Font Awesome CDN -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
 		<!-- Page Title -->
 		<title>Art Gallery - Contact</title>
 	</head>
 
-	<!-- Header-->
-	<header>
-		<!-- Navbar -->
-		<nav class="navbar navbar-dark bg-dark">
-			<a class="navbar-brand" href="index.php">Art Gallery</a>
-			<ul class="nav justify-content-end">
-				<li class="nav-item">
-					<a class="nav-link" href="gallery.php">Gallery</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="about.php">About</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="contact.php">Contact</a>
-				</li>
-			</ul>
-		</nav>
-	</header>
-
 	<!-- Body -->
 	<body>
+		<!-- Header-->
+		<header>
+			<!-- Navbar -->
+			<nav class="navbar navbar-dark bg-dark">
+				<a class="navbar-brand" href="index.php">Art Gallery</a>
+				<ul class="nav justify-content-end">
+					<li class="nav-item">
+						<a class="nav-link" href="gallery.php">Gallery</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="about.php">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="contact.php">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+		<!-- Begin Page Content -->
+		<main class="container">
+
+		</main>
+
+		<!-- Footer -->
+		<footer class="footer fixed-bottom">
+			<div class="container">
+				<a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+				<a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+			</div>
+		</footer>
+
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	</body>
-
-	<!-- Footer -->
-	<footer>
-
-	</footer>
 </html>
