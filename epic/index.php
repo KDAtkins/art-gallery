c363eb27d2c014d29f93c0ee626966bf1757d4ac
Kevin Atkins <kevindewayneatkins@gmail.com>
2018-10-16 09:29:10 -0700

    initial commit

---
+++ epic/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="description" content="main page for epic"/>
+		<link href="" rel="stylesheet" type="text/css"/>
+		<title>Epic Page</title>
+	</head>
+	<body>
+		<header>
+			<!-- Navbar -->
+			<nav class="topnav">
+				<a href="index.php">Home</a>
+				<a href="art-gallery.php">Art Gallery - Wireframe</a>
+			</nav>
+		</header>
+		<main>
+			<h1>Executive Summary</h1>
+			<p>This mobile web application will serve as a medium that displays artwork completed by the client. It will feature an Instagram widget that</p>
+		</main>
+	</body>
+	<!--	<footer>-->
+	<!---->
+	<!--	</footer>-->
+</html>
